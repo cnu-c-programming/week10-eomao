21c0ca5ef088611c4a9bec3d0dc9904cd3aac520
eomao <156084917+eomao@users.noreply.github.com>
2026-05-08 14:10:08 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -16,8 +16,7 @@ int main() {
     struct S1 s1;
     struct S2 s2;
 
-    printf("sizeof(S1): %zu\n", sizeof(s1));
-    printf("sizeof(S2): %zu\n", sizeof(s2));
+    printf("%zu %zu\n", sizeof(s1), sizeof(s2));
 
     return 0;
 }
